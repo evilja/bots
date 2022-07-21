d4b217de3c3a9ca8247456207ad6a63d3dae98ad
evil <97121235+evilja@users.noreply.github.com>
2022-07-21 13:19:46 +0300

    Create main.py

---
+++ belirli-kelimeye-belirli-cevap/main.py
@@ -0,0 +1,34 @@
+import praw
+import random
+import evilparser
+
+
+client_id     = ""
+client_secret = ""
+username      = ""
+password      = ""
+user_agent    = f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+reddit = praw.Reddit(client_id = client_id,
+                     client_secret = client_secret,
+                     username = username,
+                     password = password,
+                     user_agent = user_agent)
+
+
+
+subreddit = reddit.subreddit("herseycokguzelolacak")
+
+x = evilparser.parser()
+config = x.getconf("evilconf",verbose=True)
+def randomtextsec():
+    return config["textler"][random.randint(0,len(config["textler"])-1)]
+while True:
+
+    for comnt in subreddit.stream.comments(skip_existing=True):
+
+        if comnt.author.name.lower() in config["sikilmesi_gereken_hesaplar"]:
+
+            if config["tetikleyici"] in "".join(comnt.body.lower().split(" ")).replace(".","").replace(",",""):
+                asd = randomtextsec()
+                print(comnt.author.name+" -- "+asd[0:25])
+                comnt.reply(body=f"{asd}. ({config['tetikleyici']})\n\n^(Bu bir bot. {comnt.author.name} adlı şahısın annesini sikmek için geliştirildi)")
